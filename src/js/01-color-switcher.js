9ca2a2f764f92a89cc1d891a26fb85127a16c6e9
feoalexei <feoalexei@ukr.net>
2022-12-18 21:27:09 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const buttons = document.querySelectorAll('button[type="button"]');
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
 
+stop.disabled = true;
+
+start.addEventListener('click', startChanging);
+stop.addEventListener('click', stopChanging);
+
+function startChanging() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  toggleDisabled();
+}
+
+function stopChanging() {
+  clearInterval(timerId);
+  toggleDisabled();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function toggleDisabled() {
+  buttons.forEach(btn => {
+    btn.toggleAttribute('disabled');
+  });
+}
